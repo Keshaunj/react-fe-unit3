2baff25610b0d1b70bc5eba517121b7c2cdd3d06
Keshaunj <keshaunjones48@gmail.com>
2024-11-19 16:10:59 -0500

    fetch work towards backend

---
+++ src/phoneNumberServices.js
@@ -0,0 +1,18 @@
+const BASE_URL = 'http://localhost:3000/api/phone/validate/'; 
+
+
+const validatePhoneNumber = async (phoneNumber) => {
+  try {
+    const res = await fetch(`${BASE_URL}${phoneNumber}`);
+    if (!res.ok) {
+      throw new Error('Failed to validate phone number');
+    }
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.error('Error:', error);
+    throw error;
+  }
+};
+
+export { validatePhoneNumber };
